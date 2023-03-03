591639390d1ae2a0253b26a4f29761e35cd26d95
Marcel Robeer <marcel_robeer@hotmail.com>
2023-03-03 15:02:14 +0100

    Added mouse follow

--- header.js
+++ header.js
@@ -1,15 +1,30 @@
-$(document).ready(function() {
-    $(window).on('scroll', function() {
-    
-        var docHeight = $(document).height(),
-            winHeight = $(window).height();
-    
-        var viewport = docHeight - winHeight,
-            positionY = $(window).scrollTop();
-    
-        var indicator = ( positionY / (viewport)) * 100;
-    
-        $('.scroll-bar').css('width', indicator + '%');
-    
+class FrameService {
+    frame = null
+
+    debounce = (callback, ...args) => {
+        if (this.frame) this.cancel()
+
+        this.frame = requestAnimationFrame(() => {
+        this.frame = null
+        callback(...args)
+        })
+    }
+
+    cancel = () => {
+        cancelAnimationFrame(this.frame)
+    }
+}
+
+const frameService = new FrameService()
+
+window.addEventListener('DOMContentLoaded', function() {
+    elem = document.querySelector('.scroll-bar');
+    window.addEventListener('scroll', (event) => {
+        frameService.debounce(() => {
+            var docHeight = document.documentElement.scrollHeight;
+            var winHeight = document.documentElement.clientHeight;
+            var scrollTop = (document.body && document.body.scrollTop) ? document.body.scrollTop : document.documentElement.scrollTop;    
+            elem.style.width = (scrollTop / (docHeight - winHeight)) * 100 + '%';
+        });
     });
 });
--- scroll.js
+++ scroll.js
@@ -1,17 +1,32 @@
+const move = (e) => {
+    frameService.debounce(() => {
+        follow.style.left = e.clientX + 'px';
+        follow.style.top = e.clientY + 'px';
+    })
+};
+
+const click = (e) => {
+    frameService.debounce(() => {
+        follow.classList.remove('animating');
+        follow.classList.add('animating');
+        setTimeout(function() { follow.classList.remove('animating')}, 1000);
+    })
+}
+
 function scrolledInView( entries ) {
     var itemLoad = 0;
 
     entries.forEach( entry => {
-        if ( entry.isIntersecting ) {
-            entry.target.classList.add('in-view');
-            entry.target.classList.add('in-view-first');
-            entry.target.setAttribute('data-delay', (itemLoad * 120).toString() + "ms");
-            entry.target.style.transitionDelay = entry.target.getAttribute('data-delay');
-            itemLoad++;
-        } else {
-            entry.target.classList.remove('in-view');
-        }
-    });
+            if ( entry.isIntersecting ) {
+                entry.target.classList.add('in-view');
+                entry.target.classList.add('in-view-first');
+                entry.target.setAttribute('data-delay', (itemLoad * 120).toString() + "ms");
+                entry.target.style.transitionDelay = entry.target.getAttribute('data-delay');
+                itemLoad++;
+            } else {
+                entry.target.classList.remove('in-view');
+            }
+        })
 }
 
 let observer = new IntersectionObserver( scrolledInView, {
@@ -21,4 +36,9 @@ let observer = new IntersectionObserver( scrolledInView, {
 window.addEventListener('DOMContentLoaded', function() {
     elements = document.querySelectorAll('[data-animate]');
     elements.forEach((elem) => {observer.observe(elem)});
+
+    follow = document.getElementById('follow');
+
+    document.addEventListener('mousemove', (e) => { move(e); });
+    document.addEventListener('mousedown', (e) => { click(e); });
 });
